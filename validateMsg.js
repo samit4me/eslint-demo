09ec7b4003212edc8ff07734409b03322e30d8e9
Samuel Sharpe <samit4me@gmail.com>
2017-04-25 18:55:54 +1000

    llllllllllllll

--- validateMsg.js
+++ validateMsg.js
@@ -1,10 +1,43 @@
 'use strict';
 
 
+var fs = require('fs');
+
+var hasToString = function (x) {
+  return x && typeof x.toString === 'function';
+};
+
+
+var bufferToString = function (buffer) {
+  return hasToString(buffer) && buffer.toString();
+};
+
+
 console.log('<<<<<<<<<<<<<');
 
-process.argv.forEach(function (val, index, array) {
-  console.log(index + ': ' + val);
+
+
+function validateMsg(msg) {
+  console.log('>>>>>>>>', msg);
+  return false;
+};
+
+var commitMsgFileOrText = '.git/COMMIT_EDITMSG';
+fs.readFile(commitMsgFileOrText, function readFile(err, buffer) {
+  if(err && err.code !== 'ENOENT') {
+    throw err;
+  }
+
+  var isFile = !err;
+  var msg = (
+    isFile
+    ? bufferToString(buffer)
+    : commitMsgFileOrText
+  );
+
+  validateMsg(msg, isFile);
 });
+
+
 
 console.log('<<<<<<<<<<<<<');
